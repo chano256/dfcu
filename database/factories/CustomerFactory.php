ef3c5a744133951458260d61aae07906d1a8253d
Allan <aochan@fltug.com>
2023-04-06 09:28:03 +0300

    adds customer factory

--- database/factories/CustomerFactory.php
+++ database/factories/CustomerFactory.php
@@ -3,18 +3,35 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Faker\Factory as Faker;
 
 class CustomerFactory extends Factory
 {
     /**
      * Define the model's default state.
      *
-     * @return array
+     * @return array<string, mixed>
      */
     public function definition()
     {
+        $faker = Faker::create();
+
         return [
-            //
+            'first_name' => $faker->firstName,
+            'last_name' => $faker->lastName,
+            'phone' => $faker->phoneNumber,
         ];
+    }
+
+    /**
+     * Indicates that the customer is active
+     */
+    public function active(): Factory
+    {
+        return $this->state(function (array $attributes) {
+            return [
+                'status' => 1,
+            ];
+        });
     }
 }
