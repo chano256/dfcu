150b3e6b751bde5f784810cbb8c13542b438ebb5
Allan Ochan <allanochan@yahoo.com>
2023-04-06 23:06:16 +0300

    adds loans identifier

--- database/migrations/2023_04_06_060355_create_loans_table.php
+++ database/migrations/2023_04_06_060355_create_loans_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 class CreateLoansTable extends Migration
@@ -16,12 +17,15 @@ class CreateLoansTable extends Migration
         Schema::create('loans', function (Blueprint $table) {
             $table->id();
             $table->decimal('amount', 12, 2, true);
+            $table->string('number', 10)->unique();
             $table->date('date');
             $table->enum('status', ['outstanding', 'closed'])->default('outstanding');
             $table->unsignedBigInteger('customer_id');
 
             $table->foreign('customer_id', 'fk_loan_customer_id')->references('id')->on('customers');
         });
+
+        DB::statement("ALTER TABLE loans ADD CONSTRAINT chk_loan_number CHECK (LENGTH(number) = 10);");
     }
 
     /**
