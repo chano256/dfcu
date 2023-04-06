93cbb06daef33df8178c077b1727635916b34378
Allan <aochan@fltug.com>
2023-04-06 08:22:34 +0300

    modifies customer table and adds constraint

--- database/migrations/2023_04_05_125232_create_customers_table.php
+++ database/migrations/2023_04_05_125232_create_customers_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 class CreateCustomersTable extends Migration
@@ -18,12 +19,13 @@ class CreateCustomersTable extends Migration
             $table->string('first_name', 30);
             $table->string('last_name', 30);
             $table->string("fullname", 60)->virtualAs("(concat(first_name,' ',last_name))");
-            $table->string('number', 10)->unique();
-            $table->string('account_number', 10)->unique();
+            $table->integer('number', 10)->unique();
             $table->string('phone', 20)->unique();
             $table->boolean('status')->default(0);
             $table->timestamps();
         });
+
+        DB::statement("ALTER TABLE customers ADD CONSTRAINT chk_customer_no_length CHECK(LENGTH(number) = 10);");
     }
 
     /**
