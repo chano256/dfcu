2c42938879e239a1ec6cae2df2ee68bd75eccf03
Allan <aochan@fltug.com>
2023-04-06 09:32:18 +0300

    adds db seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Customer;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +14,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        // we assume the other endpoints at the bank created the customers
+        // therefore this API is to query the customer loan status
+        
         // \App\Models\User::factory(10)->create();
+        Customer::factory(10)->create();
     }
 }
