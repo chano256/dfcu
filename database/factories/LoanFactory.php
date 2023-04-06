117832487d220f2d8b97e8d39123bd653ac0e913
Allan Ochan <allanochan@yahoo.com>
2023-04-06 23:12:56 +0300

    modifies loan factory to create unique number

--- database/factories/LoanFactory.php
+++ database/factories/LoanFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use App\Models\Customer;
+use App\Models\Loan;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Faker\Factory as Faker;
 
@@ -16,8 +17,14 @@ class LoanFactory extends Factory
     public function definition()
     {
         $faker = Faker::create();
+
+        do {
+            $number = $faker->regexify('[0-9A-Za-z]{10}');
+        } while (Loan::whereNumber($number)->first());
+
         return [
             'amount' => $faker->numberBetween(50000, 1000000),
+            'number' => $number,
             'date' => $faker->date(),
             'status' => $faker->randomElement(['outstanding', 'closed']),
             'customer_id' => Customer::factory()->hasAccount()->create()->id
