335177387bdd9c5d758116d37f30da97ac5f6d3b
Allan Ochan <allanochan@yahoo.com>
2023-04-07 10:09:57 +0300

    adds tests for validating account numbers

--- tests/Unit/CustomerTest.php
+++ tests/Unit/CustomerTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Tests\Feature;
+namespace Tests\Unit;
 
 use App\Models\Account;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
@@ -28,9 +28,28 @@ class CustomerTest extends TestCase
         do {
             $number = $faker->numberBetween(10000000000);
         } while (Account::whereNumber($number)->first());
-        
+
         $response = $this->getJson(route('customer.account.loans.show', ['number' => $number]));
         // dd($response->__toString());
         $response->assertStatus(422);
+    }
+
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_fails_for_invalid_account_number_in_url()
+    {
+        // login
+        // $this->login();
+
+        $faker = Faker::create();
+
+        // create an account number with less than 10 digits
+        $number = $faker->numberBetween(1, 100000000);
+
+        $response = $this->getJson(route('customer.account.loans.show', ['number' => $number]));
+        $response->assertStatus(411);
     }
 }
