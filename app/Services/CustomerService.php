7c2c28a72787be1ef088ba0dd6c32b9d1e1bb5bc
Allan Ochan <allanochan@yahoo.com>
2023-04-10 11:17:46 +0300

    modifies appropriate responses

--- app/Services/CustomerService.php
+++ app/Services/CustomerService.php
@@ -21,7 +21,7 @@ class CustomerService
         // Get outstanding(current) loans
         $loans = Loan::outstanding()->with('customer')->where(['customer_id' => $account->customer_id])->get();
 
-        abort_unless(($loans->count() > 0), Response::HTTP_NOT_FOUND, 'No loans found.');
+        abort_unless(($loans->count() > 0), Response::HTTP_NO_CONTENT, 'No loans found.');
 
         Log::info("Outstanding loans generated for account {$account_number}");
 
