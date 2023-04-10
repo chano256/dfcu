e5a2501e7422091d33fdb11dd34dea0a7af167aa
Allan Ochan <allanochan@yahoo.com>
2023-04-10 14:31:27 +0300

    adds passport auth handling

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
+use Laravel\Passport\Passport;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -25,6 +26,12 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        //
+        Passport::routes();
+        // Passport::hashClientSecrets();
+
+        $expires_in = now()->addMinutes(10);
+        Passport::tokensExpireIn($expires_in);
+        Passport::refreshTokensExpireIn($expires_in);
+        // Passport::personalAccessTokensExpireIn(now()->addMonths(6));
     }
 }
